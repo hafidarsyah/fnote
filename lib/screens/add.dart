5e35961a068c601b93d163070a2ced649cbee8cb
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-17 08:15:58 +0700

    add add page

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -23,19 +23,61 @@ class _AddState extends State<Add> {
             }),
         title: Text(
           "Add",
-          style: GoogleFonts.poppins(fontWeight: FontWeight.w600),
+          style: whiteTextStyle,
         ),
       ),
       body: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Title',
-              hintText: 'Write note title',
-              fillColor: blueColor,
+          SizedBox(height: 12),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              cursorColor: blueColor,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(color: blackColor),
+                hintText: 'Write note title',
+                hintStyle: TextStyle(color: greyColor),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: blueColor)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: blueColor)),
+              ),
+              autofocus: true,
             ),
-            autofocus: true,
           ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              cursorColor: blueColor,
+              maxLines: 8,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(color: blackColor),
+                hintText: 'Write note title',
+                hintStyle: TextStyle(color: greyColor),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: blueColor)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: blueColor)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Save",
+                style: whiteTextStyle,
+              ),
+              style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(blueColor),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(horizontal: 28, vertical: 14))),
+            ),
+          )
         ],
       ),
     );
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color blackColor = Color(0xff161822);
-Color greyColor = Color(0xfff2f2f2);
+Color blackColor = Color(0xff333333);
+Color greyColor = Color(0xffc4c4c4);
 Color whiteColor = Color(0xfff9f9f9);
 Color blueColor = Color(0xff383dbf);
 Color lightBlueColor = Color(0x22383dbf);
 
-TextStyle blackTextStyle =
-    GoogleFonts.poppins(color: blackColor, fontWeight: FontWeight.w700);
+TextStyle whiteTextStyle =
+    GoogleFonts.poppins(color: whiteColor, fontWeight: FontWeight.w700);
 
 TextStyle greyTextStyle = GoogleFonts.roboto(color: greyColor);
